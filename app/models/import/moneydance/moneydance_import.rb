66004c2c30e7d603581257a170d181116f22e3d8
Adeynack <Adeynack@users.noreply.github.com>
2021-07-29 23:04:02 +0200

    split import sections

--- app/models/import/moneydance/moneydance_import.rb
+++ app/models/import/moneydance/moneydance_import.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/ClassLength
-# rubocop:disable Metrics/AbcSize
-# rubocop:disable Metrics/PerceivedComplexity
 class Import::Moneydance::MoneydanceImport
+  include Import::Moneydance::RegisterImport
+
   attr_reader :logger
   attr_reader :md_json
   attr_reader :book
@@ -75,103 +74,6 @@ class Import::Moneydance::MoneydanceImport
     @md_accounts_by_old_id ||= md_items_by_type["acct"].index_by { |a| a["old_id"] }
   end
 
-  def import_accounts
-    logger.info "Importing registers (MD accounts)"
-
-    accounts_by_id = md_items_by_type["acct"].index_by { |a| a["id"] }
-
-    accounts_by_parent_id = accounts_by_id.values.group_by { |a| a["parentid"] || :root }
-    root_accounts = accounts_by_parent_id[:root]
-    raise StandardError, "More than one root account found." if root_accounts.length > 1
-
-    root_account = root_accounts.first
-    raise StandardError, "No root account found." if root_account.blank?
-
-    first_level_accounts_per_type = accounts_by_parent_id[root_account["id"]].group_by { |a| a["type"] }
-
-    [
-      first_level_accounts_per_type.delete("i"),
-      first_level_accounts_per_type.delete("e"),
-      first_level_accounts_per_type.values.flatten,
-    ].each do |accounts_to_import|
-      types = accounts_to_import.map { |a| a["type"] }.uniq!.sort!
-      logger.info "Importer MD accounts of type#{types.many? ? 's' : ''} #{types.join(', ')}"
-
-      accounts_to_import.each do |account|
-        import_account_recursively parent_account: nil,
-                                   md_account: account,
-                                   md_accounts_by_parent_id: accounts_by_parent_id
-      end
-    end
-  end
-
-  def register_class_and_info_from_md_account(md_account)
-    case md_account["type"]
-    when "a" then [Registers::Asset, nil]
-    when "b" then [Registers::Bank, extract_bank_account_info(md_account)]
-    when "c" then [Registers::Card, extract_card_account_info(md_account)]
-    when "e" then [Registers::Expense, nil]
-    when "i" then [Registers::Income, nil]
-    when "l" then [Registers::Liability, nil]
-    when "o" then [Registers::Loan, nil]
-    when "v" then [Registers::Investment, extract_investment_info(md_account)]
-    else raise StandardError, "Unknown account type code \"#{md_account['type']}\"."
-    end
-  end
-
-  def import_account_recursively(parent_account:, md_account:, md_accounts_by_parent_id:)
-    logger.info "Importing MD '#{md_account['type']}' type account \"#{md_account['name']}\" (ID \"#{md_account['id']}\")"
-
-    register_class, account_info = register_class_and_info_from_md_account(md_account)
-    register = register_class.create!(
-      created_at: from_md_unix_date(md_account["creation_date"]),
-      name: md_account["name"].presence&.strip,
-      book: book,
-      parent: parent_account,
-      starts_at: from_md_int_date(md_account["date_created"]),
-      currency_iso_code: md_account["currid"].presence&.then { |curr_id| md_currencies_by_id[curr_id]["currid"] },
-      initial_balance: md_account["sbal"]&.then(&:to_i),
-      active: md_account["is_inactive"] != "y",
-      default_category_id: md_account["default_category"].presence&.then { |c| register_by_md_old_id[c] },
-      info: account_info,
-      notes: md_account["comment"].presence&.strip
-    )
-    md_account["old_id"].presence&.tap { |old_id| register_by_md_old_id[old_id] = register }
-    register.import_origins.create! external_system: "moneydance", external_id: md_account["id"]
-
-    (md_accounts_by_parent_id[md_account["id"]] || []).each do |child|
-      import_account_recursively parent_account: register,
-                                 md_account: child,
-                                 md_accounts_by_parent_id: md_accounts_by_parent_id
-    end
-  end
-
-  def extract_bank_account_info(md_account)
-    bank_account_number = md_account["bank_account_number"].presence
-    Registers::BankInfo.new(
-      account_number: bank_account_number,
-      iban: IBANTools::IBAN.valid?(bank_account_number) ? bank_account_number : nil
-    )
-  end
-
-  def extract_card_account_info(md_account)
-    Registers::CardInfo.new(
-      account_number: nil, # "bank_account_number" stores the card number in MD
-      bank_name: md_account["bank_name"].presence,
-      iban: nil, # "bank_account_number" stores the card number in MD
-      interest_rate: md_account["apr"].presence&.to_f,
-      credit_limit: md_account["credit_limit"].presence&.to_f&.then { |l| (l * 10).to_i },
-      card_number: md_account["bank_account_number"].presence,
-      expires_at: expiry_date(md_account["exp_year"], md_account["exp_month"])
-    )
-  end
-
-  def extract_investment_info(md_account)
-    Registers::InvestmentInfo.new(
-      account_number: md_account["invst_account_number"]
-    )
-  end
-
   def expiry_date(exp_year, exp_month)
     return nil if exp_year.blank?
 
@@ -180,6 +82,3 @@ class Import::Moneydance::MoneydanceImport
     Date.new(year, month, 1)
   end
 end
-# rubocop:enable Metrics/ClassLength
-# rubocop:enable Metrics/AbcSize
-# rubocop:enable Metrics/PerceivedComplexity
