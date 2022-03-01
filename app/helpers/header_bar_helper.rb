78996d304b99d44946455076ca62ba28817ab877
Adeynack <Adeynack@users.noreply.github.com>
2022-03-01 23:24:12 +0100

    de-bootstrap the app

--- app/helpers/header_bar_helper.rb
+++ app/helpers/header_bar_helper.rb
@@ -1,18 +1,10 @@
 # frozen_string_literal: true
 
 module HeaderBarHelper
-  def nav_item_link_to(text, path)
-    content_tag "li", class: "nav-item" do
-      link_to_unless_current text, path, class: "nav-link" do
-        content_tag "span", text, class: "nav-link active", "aria-current": "page"
-      end
-    end
-  end
-
-  def dropdown_menu_link_to(text, path)
-    content_tag "li" do
-      link_to_unless_current text, path, class: "dropdown-item" do
-        content_tag "span", text, class: "dropdown-item"
+  def nav_item_link_to(text, path, title: nil)
+    content_tag "span", class: "nav-item" do
+      link_to_unless_current text, path, class: "nav-link", title: do
+        content_tag "span", text, class: "nav-text active", title:
       end
     end
   end
